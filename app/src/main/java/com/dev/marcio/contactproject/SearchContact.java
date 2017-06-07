89d09395e44b377db039e59b94e026b9510262c0
marcio prando <mprandot@gmail.com>
2017-06-07 00:19:58 -0300

    creating a list with filter

--- app/src/main/java/com/dev/marcio/contactproject/SearchContact.java
+++ app/src/main/java/com/dev/marcio/contactproject/SearchContact.java
@@ -2,6 +2,14 @@ package com.dev.marcio.contactproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import beans.Contact;
 
 public class SearchContact extends AppCompatActivity {
 
@@ -9,5 +17,31 @@ public class SearchContact extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_contact);
+
+
+        final EditText search = (EditText) findViewById(R.id.edtSearch);
+
+        search.addTextChangedListener(new TextWatcher() {
+
+            public void afterTextChanged(Editable s) {}
+
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                Log.d(getClass().getSimpleName(), s.toString());
+            }
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+        });
+
+//        search.addTextChangedListener(fieldValidatorTextWatcher);
+//
+//        ListView listView = (ListView) findViewById(R.id.listContacts);
+//
+//        ArrayAdapter<Contact> adapter = new ArrayAdapter<Contact>(this,
+//                android.R.layout.simple_list_item_1, usuarios);
+//        listView.setAdapter(adapter);
+
+
     }
+
+
 }
